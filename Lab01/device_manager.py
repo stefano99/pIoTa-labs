43615649c3706f45bc1715a5ff758a4eac07acf6
Stefano Falchi <falchi.stefi@hotmail.it>
2025-10-05 19:51:19 +0200

    added logic for adding measureType, . availableServices and servicesDetails to the devicesList

--- Lab01/device_manager.py
+++ Lab01/device_manager.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 # import device as device
 
 class DeviceManager:
@@ -78,31 +79,68 @@ class DeviceManager:
             print("No device found with that measure type.")
         return found
 
-
+# INCOMPLETE
     def insertDevice(self):
         user_input_id = input("Enter the new Device ID: ")
         devices_list = self.catalog["devicesList"]
         found = False
+
+        # check if device with same ID already exists
         for device in devices_list:
-            if device["ID"] == user_input_id:
+            if device["deviceID"] == user_input_id:
                 found = True
+
+        # if the device ID already exists, ask user for update
         if found:
             print("Device with this ID already exists.")
+
+        # if the device ID does not exist, ask user for device details
         else:
             user_input_name = input("Enter the new Device Name: ")
+
+            measure_types = []
             available_services = []
+            service_details = []
+
+
+            # ask user for available measureTypes until they type 'ok'
             while True:
-                service_to_enter = "Please add the serviceName available. When finish type ok"
+                measure_type_to_enter = input("Please add the measureType available. When finish type ok: ")
+                if measure_type_to_enter == "ok":
+                    break
+                measure_types.append(measure_type_to_enter)
+
+            # ask user for available services until they type 'ok'
+            while True:
+                service_to_enter = input("Please add the serviceType available. When finish type ok: ")
                 if service_to_enter == "ok":
                     break
-                available_services.append(available_services)
-        device_dict = {"deviceName":user_input_name,"deviceID":user_input_id,"availableServices":available_services}
+                single_detail_to_enter = []
+                # for each service, ask user for serviceDetails until they type 'ok'
+                while True:
+                    detail_to_enter = input("Please add the serviceDetails available for the Service you just added. When finish type ok: ")
+                    if detail_to_enter == "ok":
+                        break
+                    single_detail_to_enter.append(detail_to_enter)
+                if service_to_enter == "MQTT":
+                    single_service_details_to_enter = {"serviceType":service_to_enter,"topic":single_detail_to_enter}
+                elif service_to_enter == "REST":
+                    single_service_details_to_enter = {"serviceType":service_to_enter,"serviceIP":single_detail_to_enter}  
+                service_details.append(single_service_details_to_enter)
+                available_services.append(service_to_enter)
+                
+
+
+        device_dict = {"deviceID":user_input_id,"deviceName":user_input_name,"measureType":measure_types,"availableServices":available_services}
         self.catalog["devicesList"].append(device_dict)
+
+        # update lastUpdate field to current date and time
+        self.catalog["lastUpdate"] = datetime.now().strftime("%Y-%m-%d %H:%M")
 
     def printAll(self):
         print(self.catalog)
         
     def exit(self):
         print("Exiting the Device Manager.\nSaving catalog to catalog.json")
-        json.dump(self.catalog,open("catalog.json","w"))
+        json.dump(self.catalog,open("catalog_updated.json","w"))
         exit(0)
